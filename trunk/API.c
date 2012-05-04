0044015ca22b01ee822a51a8c7329f49e4e27ab9
questomago <questomago@gmail.com>
2012-05-04 20:35:29 +0000

    Se agregaron los archivos al repositorio y se escribió lo básico

---
+++ trunk/API.c
@@ -0,0 +1,31 @@
+#include "API.h"
+
+
+typedef struct ENetwork{
+/*aqui van los parametros espesificos*/
+}EstadoNetworkSt;
+
+
+EstadoNetwork networkNuevo(int Verbosidad){
+	EstadoNetwork Net;
+return Net;
+}
+
+int chauNetwork(EstadoNetwork N){
+	int n;
+return n;
+}
+
+int leerUnLado(EstadoNetwork N){
+	int n;
+return n;
+}
+
+int aumentarFlujo(EstadoNetwork N){
+	int n;
+return n;
+}
+
+void imprimirFlujoMaximal(EstadoNetwork N){
+
+}
