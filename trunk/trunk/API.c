a80b91c88a550a179a1cddeeb0fbb298a637eb48
questomago@gmail.com <questomago@gmail.com@f49af6b9-9fa8-0c8e-6eb1-041f54daea09>
2012-05-05 04:14:55 +0000

    "hecho" el main... o un pedazo!

--- trunk/trunk/API.c
+++ trunk/trunk/API.c
@@ -1,9 +1,17 @@
 #include "API.h"
 
-
 typedef struct ENetwork{
-/*aqui van los parametros espesificos*/
+	u32 vflujo; /*valor del flujo del network(auxiliar o no)*/
+	cuadruple * edge; /* ordenado por BFS*/
+	u32 *S; /*corte*/
 }EstadoNetworkSt;
+
+typedef struct cuadrupleSt{
+	u32 father;
+	u32 son;
+	u32 init_cap;
+	u32 curr_cap;
+} *cuadruple;
 
 
 EstadoNetwork networkNuevo(int Verbosidad){
