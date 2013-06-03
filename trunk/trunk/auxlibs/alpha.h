10194bb148a04b66b312a3540696d4c9d1a2bc77
rezequielp@gmail.com <rezequielp@gmail.com@f49af6b9-9fa8-0c8e-6eb1-041f54daea09>
2013-06-03 09:17:07 +0000

    Modificaciones en stack y queue

--- trunk/trunk/auxlibs/alpha.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALPHA_H
-#define ALPHA_H
-
-#include "../edge.h"
-
-/* AQUI se puede modificar el tipo de elementos de stack*/
-typedef edge alpha;
-
-#endif
--- trunk/trunk/auxlibs/stack.h
+++ trunk/trunk/auxlibs/stack.h
@@ -1,23 +1,29 @@
 #ifndef STACK_H
 #define STACK_H
 
-typedef struct StackSt *stack;
+typedef struct StackSt *Stack;
 
 
-stack stack_new (void);
+Stack stack_create(void);
 /*Pila vacia*/
 
-void stack_destroy(stack p);
+void stack_destroy(Stack S);
 /*Destuctor de pila*/
 
-int stack_push(stack p, void elem);
+int stack_push(Stack S, void elem);
 /*Agrega un elemento*/
 
-void stack_pop(stack p);
+void stack_pop(Stack S);
 /*Elimina el ultimo elemento agregado*/
 
-int stack_is_empty(stack p);
+int stack_isEmpty(Stack S);
 /*Consulta si la pila es vacia o no*/
 
-void stack_top(stack p);
+void stack_top(Stack S);
 /*Muestra el ultimo elemento agregado a la pila*/
+
+int stack_size(Stack S);
+/*Retorna la cantidad de elementos de la pila*/
+
+int stack_revert (Stack S1, Stack S2 );
+/*Devuelve en S2 la pila S1 revertida*/
